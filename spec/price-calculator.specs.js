3cc439f88bac14d6d2358595591431c0ac085df6
ebimammadi <ebi@tadbir.net>
2020-01-24 12:26:23 +0100

    Two other cases were added to the price calculator in order to test other condition of inputs

--- spec/price-calculator.specs.js
+++ spec/price-calculator.specs.js
@@ -1,12 +1,41 @@
 describe('price calculator', function(){
   it('should calc price right', function(){
-    var userType = 0;
-    var productType = 0;
-    var price = 1;
-    var publishedDate = new Date();
+    const userType = 0;
+    const productType = 0;
+    const price = 1;
+    const publishedDate = new Date();
 
-    var expected = 16;
-    var actual = PriceCalculator.calculatePrice(userType, productType, price, publishedDate);
+    const expected = 16;
+    const priceCalculator = new PriceCalculator();
+    const actual = priceCalculator.calculatePrice(userType, productType, price, publishedDate);
+    expect(expected).to.equal(actual);
+  });
+});
+
+describe('price calculator case 2', function(){
+  it('should calc price right (case 2)', function(){
+    const userType = 1;
+    const productType = 1;
+    const price = 10;
+    const publishedDate = new Date();
+
+    const expected = 40;
+    const priceCalculator = new PriceCalculator();
+    const actual = priceCalculator.calculatePrice(userType, productType, price, publishedDate);
+    expect(expected).to.equal(actual);
+  });
+});
+
+describe('price calculator case 3 - Different Date', function(){
+  it('should calc price right (case 2)', function(){
+    const userType = 1;
+    const productType = 0;
+    const price = 100;
+    const publishedDate = new Date("2020-01-01");
+
+    const expected = 120;
+    const priceCalculator = new PriceCalculator();
+    const actual = priceCalculator.calculatePrice(userType, productType, price, publishedDate);
     expect(expected).to.equal(actual);
   });
 });
